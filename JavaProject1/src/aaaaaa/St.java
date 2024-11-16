d092dc74ff2db2a6de41a196f2e38f912192b2f0
ajinkya-ops <ajinkya1jk@gmail.com>
2024-11-16 22:03:51 +0530

    Update St.java
    Added new entry

--- JavaProject1/src/aaaaaa/St.java
+++ JavaProject1/src/aaaaaa/St.java
@@ -5,6 +5,8 @@ public class St {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Ajinkya");
+System.out.println("Rajesh");
+System.out.println("Hari");
 	}
 
 }
